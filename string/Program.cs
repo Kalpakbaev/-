10fb2f4695ba5abd91eb7e532c2de6abaf2392f6
Kalpakbaev <kozha_1992@mail.ru>
2023-01-29 23:39:10 +0500

    Last example

---
+++ string/Program.cs
@@ -0,0 +1,16 @@
+﻿
+string[] Array(){
+Random myRandom = new Random();
+string [] array = new string[3];
+for(int i = 0; i < array.Length; i++){
+    Console.WriteLine("Введите слово");
+     array[i] = Convert.ToString(Console.ReadLine());
+}
+return array;
+}
+void ShowArray(string[] array){
+ for(int i = 0 ; i < array.Length; i++){
+    Console.Write($"{array[i]}, ");
+ }
+}
+ShowArray(Array());
